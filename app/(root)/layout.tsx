2a3507898eabc2e08c3d11376dc1cde3d1e67c54
yashdevpl <yash.doifode@pi-labs.ai>
2026-01-01 14:54:35 +0530

    feat : vercel analytics added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import { Analytics } from "@vercel/analytics/next"
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div>{children}</div>
+        <div>{children}
+            <Analytics /></div>
     )
 }
 
